9f8000080aa0623aa2c5217b6d4b6c36f0796e40
Rekha-sys0 <rekha@sysorexedge.com>
2021-02-24 09:33:56 +0530

    initil commit

---
+++ app/Http/Controllers/MyHomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MyHomeController extends Controller
+{
+   public function index(){
+      echo "myHomeController";
+   }
+   
+}
---
+++ app/Http/Controllers/myNewController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\myNewModel;
+use Illuminate\Http\Request;
+
+class myNewController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\myNewModel  $myNewModel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(myNewModel $myNewModel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\myNewModel  $myNewModel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(myNewModel $myNewModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\myNewModel  $myNewModel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, myNewModel $myNewModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\myNewModel  $myNewModel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(myNewModel $myNewModel)
+    {
+        //
+    }
+}
